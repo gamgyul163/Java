2159702e9b5a0ebccf62b6baff6749ea1d368016
gamgyul163 <gamgyul163@gmail.com>
2023-11-01 21:32:10 +0900

    [Add] 구구단 출력

---
+++ Exercise/TimesTable.java
@@ -0,0 +1,15 @@
+public class TimesTable {
+    public static void main(String[] args) {
+        System.out.println("[구구단 출력]");
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.print(String.format("%02d X %02d = %02d", j, i, j*i));
+                if (j < 9) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("\n");
+                }
+            }
+        }
+    }
+}
