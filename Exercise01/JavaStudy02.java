9f8630f8e534705159074e8db52640ba31f36d3f
gamgyul163 <gamgyul163@gmail.com>
2023-11-09 14:37:26 +0900

    [Fix] 미니과제2 리팩토링

--- Exercise01/JavaStudy02.java
+++ Exercise01/JavaStudy02.java
@@ -4,21 +4,45 @@ import java.util.Scanner;
 
 public class JavaStudy02 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        CashBackCalculator calculator = new CashBackCalculator(scanner);
+
         System.out.println("[캐시백 계산]");
 
-        System.out.print("결제 금액을 입력해 주세요.(금액):");
-        Scanner scanner = new Scanner(System.in);
-        int paymentAmount = scanner.nextInt();
-        scanner.close();
+        calculator.printCashBackAmount(calculator.askPaymentAmount());
+    }
+}
 
-        int cashBackRatio = 10;
-        int cashBackAmount = paymentAmount*cashBackRatio/100;
+class CashBackCalculator { // 캐시백 계산기
+    private final Scanner SCANNER;
+    private final int CASH_BACK_RATIO = 10; // 캐시백 비율
+    private final String INPUT_ERROR_MESSAGE = "잘못된 입력입니다. 다시 입력해주세요";
+    CashBackCalculator(Scanner scanner) {
+        this.SCANNER = scanner;
+    }
+
+    int askPaymentAmount() { // 입력 유도 및 유효성 검증
+        int numericInput;
+        do {
+            System.out.print("결제 금액을 입력해 주세요.(금액):");
+            String strInput = SCANNER.nextLine().trim();
+            if(strInput.matches("\\d+")) {
+                numericInput = Integer.parseInt(strInput);
+                break;
+            }
+            System.out.println(INPUT_ERROR_MESSAGE);
+        } while (true);
+        return numericInput;
+    }
+    private int calCashBackAmount(int paymentAmount) { // 캐시백 계산
+        int cashBackAmount = paymentAmount*CASH_BACK_RATIO/100;
         cashBackAmount -= cashBackAmount%100; // 100이하 절삭
         if (cashBackAmount > 300) {
             cashBackAmount = 300;
         }
-
-        System.out.printf("결제 금액은 %d원이고, 캐시백은 %d원 입니다.", paymentAmount, cashBackAmount);
+        return cashBackAmount;
+    }
+    void printCashBackAmount(int paymentAmount) { // 캐시백 계산 결과 출력
+        System.out.printf("결제 금액은 %d원이고, 캐시백은 %d원 입니다.",paymentAmount, calCashBackAmount(paymentAmount));
     }
 }
-
